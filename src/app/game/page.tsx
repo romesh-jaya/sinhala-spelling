b2caa985f004097e49d9c234f0c3807a2a37e6af
Romesh <romesh@medoment.com>
2025-04-18 13:08:35 +0530

    1

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Game from "../../../components/Game/Game";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./page.scss";
 
 export default function GamePage() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import "./page.scss"; // Import the SCSS file for page-specific styles
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
