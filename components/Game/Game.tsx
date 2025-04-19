cf10832f7332ac2b242626e1d98d3d1a6d4ac94e
Romesh <romesh@medoment.com>
2025-04-19 10:37:30 +0530

    1

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 import Keyboard from '../Keyboard/Keyboard';
 import Carousel from '../Carousel/Carousel';
 import { sinhalaLettersLevelBasic, MAX_GAME_SLIDES } from '@/constants';
@@ -125,7 +126,15 @@ const Game: React.FC<GameProps> = ({ onGameComplete }) => {
 
   return (
     <div className="game-container">
-      <h1 className="game-title">Sinhala Spelling Game</h1>
+      <div className="game-header">
+        <Link href="/" className="home-button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+          </svg>
+        </Link>
+        <h1 className="game-title">Sinhala Spelling Game</h1>
+      </div>
       {showInvalidLevelWarning && (
         <div className="warning-message">
           Invalid level
