cc1de4de54d54d9faea23c57699a4c07ed1bdf67
Romesh <romesh@medoment.com>
2026-02-07 09:13:07 +0530

    icon change

--- components/TileDisplay/TileDisplay.tsx
+++ components/TileDisplay/TileDisplay.tsx
@@ -63,7 +63,10 @@ const TileDisplay: React.FC<TileDisplayProps> = ({
               title="Play audio"
               aria-label="Play audio for this word"
             >
-              🔊
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+                <path d="M15.54 8.46a7 7 0 0 1 0 9.9M19.07 4.93a11 11 0 0 1 0 15.66"></path>
+              </svg>
             </button>
           </>
         )}
